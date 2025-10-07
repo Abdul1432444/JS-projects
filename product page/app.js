6452ee17d6f2026fb44552980ae0d0229b885cdc
Abdul1432444 <ar9834606@gmail.com>
2025-10-07 19:49:18 +0500

    done

--- product page/app.js
+++ product page/app.js
@@ -1,4 +1,26 @@
 let productImg = document.getElementById("productImg");
 let btn = document.getElementsByClassName("btn");
 
-btn[0].onclick = function () {};
+btn[0].onclick = function () {
+  productImg.src = "images/product-1.jpg";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[1].onclick = function () {
+  productImg.src = "images/product-2.jpg";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[2].onclick = function () {
+  productImg.src = "images/product-3.jpg";
+  for (bt of btn) {
+    bt.classList.remove("active");
+  }
+  this.classList.add("active");
+};
+productImg.src = "images/product-1.jpg";
+btn[0].classList.add("active");
