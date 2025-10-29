e872ecf7fe625d21353e1badc81ef374d5d217b0
Abdul1432444 <ar9834606@gmail.com>
2025-10-29 11:09:19 +0500

    done

---
+++ calculator/script.js
@@ -0,0 +1,5 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+const themeTonggle = document.getElementById("theme-toggle");
+
+let currentInput = "";
