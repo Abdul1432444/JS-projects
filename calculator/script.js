0beaf2c3a9b92bd49926c2a04f9f8490508d7046
Abdul1432444 <ar9834606@gmail.com>
2025-10-29 11:34:01 +0500

    done

--- calculator/script.js
+++ calculator/script.js
@@ -3,3 +3,20 @@ const buttons = document.querySelectorAll(".btn");
 const themeTonggle = document.getElementById("theme-toggle");
 
 let currentInput = "";
+
+// step 1 is handle buttons
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click ", () => handleInput(btn.dataset.key));
+});
+
+// step 2 is to handle keyboardkeys
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+  const allowedKeys = "0123456789+-*/.%";
+  if (allowedKeys.includes(key)) handleInput(key);
+  else if (allowedKeys === "enter") handleInput("=");
+  else if (allowedKeys === "backspace") handleInput("DEL");
+  else if (allowedKeys === "")
+});
